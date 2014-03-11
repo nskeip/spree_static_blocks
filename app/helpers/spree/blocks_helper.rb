307878c530660c7ac194291b2fe7622c0495f1dc
Nikita Hismatov <hismatovns@gmail.com>
2014-03-11 17:25:38 +0600

    html safe block text

--- app/helpers/spree/blocks_helper.rb
+++ app/helpers/spree/blocks_helper.rb
@@ -1,7 +1,12 @@
 module Spree
   module BlocksHelper
     def static_block(code)
-      "STATIC BLOCK TEXT by code #{code}"
+      block = Block.find_or_create_by_code(code)
+      if block.text?
+        block.text.html_safe
+      else
+        ""
+      end
     end
   end
 end
